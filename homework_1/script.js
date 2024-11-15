356dc340df1559a6f0fd73e188001cbdf3259622
Heyko Daniil <danek3661@gmail.com>
2024-11-15 21:29:34 +0200

    edited homework

--- homework_1/script.js
+++ homework_1/script.js
@@ -11,14 +11,20 @@ console.log(a + b);
 console.log(a - b);
 console.log(a / b);
 console.log(a * b);
+console.log(a ** 2);
+console.log(Math.sqrt(a))
 
 // 2. Створіть змінну довільного типу. Проведіть наступні перетворення з нею:
 
 let c = 6;
 console.log(c)
+console.log(typeof c)
 c = '6';
 console.log(c)
+console.log(typeof c)
 c = 8;
 console.log(c)
+console.log(typeof c)
 c = true;
 console.log(c)
+console.log(typeof c)
