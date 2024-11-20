4b84dbcddbb2588c558953fdf9380c5b25326bad
Heyko Daniil <danek3661@gmail.com>
2024-11-20 11:32:09 +0200

    did homework_1

---
+++ homework_1/script_1.js
@@ -0,0 +1,29 @@
+'use strict'
+
+
+// 1. Створіть 2 змінні з типом number. Проведіть з ними такі математичні операції:  
+
+let a = 3;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+console.log(a ** 2);
+console.log(Math.sqrt(a))
+
+// 2. Створіть змінну довільного типу. Проведіть наступні перетворення з нею:
+
+let c = 6;
+console.log(c);
+console.log(typeof(c));
+c = String(c);
+console.log(c);
+console.log(typeof(c));
+c = Number(c);
+console.log(c);
+console.log(typeof(c));
+c = Boolean(c);
+console.log(c);
+console.log(typeof(c));
