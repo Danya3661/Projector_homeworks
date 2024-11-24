8569b4c768ef3f4a42a6769baa2efeffdfa8d4c9
Heyko Daniil <danek3661@gmail.com>
2024-11-24 16:55:59 +0200

    edited homework_3

--- homework_3/script_3.js
+++ homework_3/script_3.js
@@ -18,14 +18,14 @@ console.log(recursiveOddSumTo(10));
 // 2. Задача про ітерацію
 
 function iterativeOddSumTo(number) {
-	for (i = 0; i <= 0; i++) {
-		if (number % 2 !== 0) {
-			return number + recursiveOddSumTo(number - 2);
-		} else {
-			return recursiveOddSumTo(number - 1);
+	let sum = 0;
+	for (let i = 0; i <= number; i++) {
+		if (i % 2 !== 0) {
+			sum = sum + i;
 		}
 	}
+	return sum;
 }
 
-console.log(iterativeOddSumTo(1)); 
-console.log(iterativeOddSumTo(10)); 
+console.log(iterativeOddSumTo(1)); // 1
+console.log(iterativeOddSumTo(10)); // 25
